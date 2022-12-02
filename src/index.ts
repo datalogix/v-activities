c2fd1f990b53e4509cbd776a93e11c15c98c61d1
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-12-02 12:00:51 -0300

    build: export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import Activity from './activities/Activity.vue'
+import MultipleChoice from './activities/MultipleChoice/Index.vue'
+
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import './assets/style.css'
 
-export { default as Activity } from './activities/Activity.vue'
-export { default as MultipleChoice } from './activities/MultipleChoice/Index.vue'
+export {
+  Activity,
+  MultipleChoice
+}
