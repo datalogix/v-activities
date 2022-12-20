6c9cf7fa70bdf455110473f0057f4fca7b2653b0
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-12-20 15:33:33 -0300

    fix: types

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,7 +1,3 @@
-/// <reference types="vite/client" />
-
-export {}
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
