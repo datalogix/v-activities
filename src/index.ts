06435d1ff5a7625caff3be455e0adf90a66398e3
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-02-08 17:48:13 -0300

    chore: export all activities

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import Activity from './activities/Activity.vue'
+import AssembleWord from './activities/AssembleWord/Index.vue'
+import ConnectTheDots from './activities/ConnectTheDots/Index.vue'
+import Crossword from './activities/Crossword/Index.vue'
+import DragAndDrop from './activities/DragAndDrop/Index.vue'
+import FillInTheBlanks from './activities/FillInTheBlanks/Index.vue'
+import Hangman from './activities/Hangman/Index.vue'
+import MemoryGame from './activities/MemoryGame/Index.vue'
 import MultipleChoice from './activities/MultipleChoice/Index.vue'
+import Paint from './activities/Paint/Index.vue'
+import WordSearch from './activities/WordSearch/Index.vue'
 
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
@@ -7,5 +16,14 @@ import './assets/style.css'
 
 export {
   Activity,
-  MultipleChoice
+  AssembleWord,
+  ConnectTheDots,
+  Crossword,
+  DragAndDrop,
+  FillInTheBlanks,
+  Hangman,
+  MemoryGame,
+  MultipleChoice,
+  Paint,
+  WordSearch
 }
