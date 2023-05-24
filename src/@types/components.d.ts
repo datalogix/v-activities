48073e48056b63526d9117ef244eb95e4b6bdb2f
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-05-24 12:53:48 -0300

    chore: update deps

--- src/@types/components.d.ts
+++ src/@types/components.d.ts
@@ -9,18 +9,18 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Action: typeof import('./../components/Action.vue')['default']
     Button: typeof import('./../components/Button.vue')['default']
     Confirmation: typeof import('./../components/Confirmation.vue')['default']
     Dead: typeof import('./../components/Dead.vue')['default']
-    Error: typeof import('./../components/Error.vue')['default']
     Footer: typeof import('./../components/Footer.vue')['default']
     Header: typeof import('./../components/Header.vue')['default']
-    Lifes: typeof import('./../components/Lifes.vue')['default']
     Loading: typeof import('./../components/Loading.vue')['default']
     Main: typeof import('./../components/Main.vue')['default']
+    Message: typeof import('./../components/Message.vue')['default']
     Modal: typeof import('./../components/Modal.vue')['default']
+    Resets: typeof import('./../components/Resets.vue')['default']
     Result: typeof import('./../components/Result.vue')['default']
+    Section: typeof import('./../components/Section.vue')['default']
     Timer: typeof import('./../components/Timer.vue')['default']
   }
 }
